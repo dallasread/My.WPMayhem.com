7d1e505f73d28559a0fd76d78de4f5e69c89d40e
Dallas Read <dallas@excitecreative.ca>
2014-11-18 13:43:26 -0400

    Duplicate user auth.

---
+++ app/routes/dashboard.js
@@ -0,0 +1,70 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+	beforeModel: function(transition) {
+		this.controllerFor('login').send('redirectIfNotAuthenticated', transition);
+	},
+	setupController: function(controller) {
+    controller.set('installations', this.store.findAll('installation'));
+  },
+	actions: {
+		register: function() {
+			if (this.get('email') && this.get('password')) {
+				window.firebase.createUser({
+					email: this.get('email'),
+					password: this.get('password')
+				}, function(error) {
+				  if (error === null) {
+				    console.log("User created successfully");
+						//this.transitionToRoute('login');
+				  } else {
+				    console.log("Error creating user:", error);
+				  }
+				});
+			} else {
+				alert("Use valid data.");
+			}
+		},
+		redirectIfNotAuthenticated: function(transition) {
+			if (!this.get('session.auth')) {
+			  this.set('previousTransition', transition);
+				this.transitionToRoute('login');
+			}
+		},
+		redirectIfAuthenticated: function() {
+			if (this.get('session.auth')) {
+				this.transitionToRoute('dashboard');
+			}
+		},
+		login: function() {
+			var e = this;
+				
+			if (this.get('email') && this.get('password')) {
+
+				window.firebase.authWithPassword({
+				  email: this.get('email'),
+				  password: this.get('password')
+				}, function(error) {
+				  if (error === null) {
+				    var userController = e.get('controllers.user');
+					  var previousTransition = userController.get('previousTransition');
+						if (previousTransition) {
+							userController.set('previousTransition', null);
+							previousTransition.retry();
+						} else {
+							e.transitionToRoute('dashboard');
+						}
+				  } else {
+				    console.log("Error authenticating user:", error);
+				  }
+				});
+			} else {
+				alert("enter valid data");
+			}
+		},
+		logout: function() {
+			window.firebase.unauth();
+			this.transitionToRoute('login');
+		}
+	}
+});
