eff5fe2cc4c6195f3e0a1d94165764c11f101d12
Dallas Read <dallas@excitecreative.ca>
2014-12-01 10:17:40 -0400

    reorder.

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -1,5 +1,11 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
-	needs: ['login']
+export default Ember.ObjectController.extend({
+	needs: ['login', 'dashboard'],
+	installations: (function() {
+    return Ember.ArrayProxy.createWithMixins(Ember.SortableMixin, {
+      sortProperties: ['name'],
+      content: this.get('session.user.installations')
+    });
+  }).property('session.user.installations')
 });
